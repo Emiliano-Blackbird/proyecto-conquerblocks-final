2e8511de3243f03f2ac3ee92771588e0d6471ccb
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-05-15 09:45:41 +0200

    Github page modificación

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/proyecto-conquerblocks-final/', // ✅ Mueve esta línea aquí, arriba de build
+  base: "/proyecto-conquerblocks-final/",
   build: {
     rollupOptions: {
       input: {
